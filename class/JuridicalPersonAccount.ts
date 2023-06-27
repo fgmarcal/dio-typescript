f1c33842e75f1434a2fbe7f8ed4a4f47f89ff406
Felipe Garcia Marçal <90734397+fgmarcal@users.noreply.github.com>
2023-06-27 10:41:05 -0300

    Update JuridicalPersonAccount.ts

--- class/JuridicalPersonAccount.ts
+++ class/JuridicalPersonAccount.ts
@@ -1,1 +1,12 @@
+import { Account } from "./Account"
 
+export class JuridicalPerson extends Account {
+
+  constructor(name: string, accountNumber: number){
+    super(name, accountNumber)
+  }
+
+  getLoan = (): void => {
+    console.log('Voce pegou um empréstimo')
+  }
+}
